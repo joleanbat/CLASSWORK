e5d80fc25526fb6130ce42cd6f43ad42a82b4657
joleanbat <jojofcbhack900@gmail.com>
2025-11-18 16:24:18 +0200

    Info on js

--- 04_HtmlElements/index.js
+++ 04_HtmlElements/index.js
@@ -1,5 +1,12 @@
+//--page to naviagte WHEN CLICKED MEMNU ITEM
+//page:  From HTML CLICK example /page:/01/demos/index.html
 function loadPage(page) {
+
+    //--get Reference fro the HTML ELEMENT BY ITS ID
+    //--contentFrame is iframe element type
     let iframgeElement = document.getElementById("contentFrame");
+
+    //--Give The IFRAME the HTML ADDRESS
     iframgeElement.src = page;
 
     // Close sidebar on mobile
